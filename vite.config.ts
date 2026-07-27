d61cbd5a82aef11ce41c5edfb25d5c5c99448056
akikiya <2775686007@qq.com>
2026-07-27 11:29:24 +0800

    feat: add sitemap generation plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+    sitemap({
+      hostname: 'https://2048board.xmit.dev/'
+    })
+  ]
 })
